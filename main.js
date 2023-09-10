c4de46221ef7efcf34b62928622b9df64650d528
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-09-11 00:33:15 +0900

    wip

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onkeydown = (e) => {
+  if (e.key === "l" || e.key === "n") {
+    const currentUID = 1;
+    const swageUID = 0;
+    if (currentUID !== swageUID) {
+      alert("ほげ");
+    }
+  }
+};
